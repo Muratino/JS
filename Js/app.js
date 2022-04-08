06ade54dc0a5e4f2a3a93592d1d3a74104952e7c
Murat <bokonovm2@gmail.com>
2022-04-08 13:09:16 +0200

    First comit

---
+++ Js/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let category = 'toys';
+
+
+// console.log(`https://someurl.com/${category}/5`);
+
+
+
+// const user = "Vana";
+
+// alert(`Привеt ${user}` );
+
+
+// let nl  = 10,
+//     dr  = 10;
+
+
+// nl++;
+// dr--;
+
+// console.log(++nl);
+// console.log(--dr);
+
+
+console.log(5*2 === 10);
+
+
+const checed = true,
+    hClose = false;
+
+
+console.log(checed || hClose);
